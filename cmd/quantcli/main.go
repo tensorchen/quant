72df5ac81a227c6ae96d001e689ea7e17e5a33ba
Chen Yixiao <tensorchen@gmail.com>
2024-04-13 01:46:41 +0800

    impi quantcli utils

---
+++ cmd/quantcli/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+
+	quantcfg "github.com/tensorchen/quant/config"
+
+	"github.com/longportapp/openapi-go/config"
+	"github.com/longportapp/openapi-go/trade"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var cfg quantcfg.Config
+	if err = yaml.Unmarshal(data, &cfg); err != nil {
+		log.Fatalln(err)
+	}
+
+	conf, err := config.New(config.WithConfigKey(cfg.LongBridge.AppKey, cfg.LongBridge.AppSecret, cfg.LongBridge.AccessToken))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tc, err := trade.NewFromCfg(conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rsp, err := tc.StockPositions(context.Background(), []string{"ARM.US"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, sps := range rsp {
+		for _, sp := range sps.Positions {
+			slog.Any("stock", sp)
+		}
+	}
+}
